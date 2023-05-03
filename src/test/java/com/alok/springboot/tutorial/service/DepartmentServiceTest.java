6752b07866cdd55f418365aa4f5b26f89893cea0
alok <galok8749@gmail.com>
2023-05-03 18:42:37 +0530

    Initial Commit

---
+++ src/test/java/com/alok/springboot/tutorial/service/DepartmentServiceTest.java
@@ -0,0 +1,43 @@
+package com.alok.springboot.tutorial.service;
+
+import com.alok.springboot.tutorial.entity.Department;
+import com.alok.springboot.tutorial.repository.DepartmentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DepartmentServiceTest {
+
+    @Autowired
+    private DepartmentService departmentService;
+    @MockBean
+    private DepartmentRepository departmentRepository;
+    @BeforeEach
+    void setUp() {
+        Department department = Department.builder()
+                .departmentName("IT")
+                .departmentCode("IT-06")
+                .departmentAddress("Bangalore")
+                .departmentId(1L)
+                .build();
+
+        Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("IT"))
+                .thenReturn(department);
+    }
+
+    @Test
+    @DisplayName("Get Data based on Department Name")
+    public void whenValidDepartmentName_thenDepartmentShouldFound() {
+        String departmentName = "IT";
+        Department found = departmentService.fetchDepartmentByName(departmentName);
+
+        assertEquals(departmentName, found.getDepartmentName());
+    }
+}
